61db457f097f77f8b088015c4322ad542d68a7ab
Krzysztof Cieśla <krzysztof.marcin.ciesla@cern.ch>
2026-03-05 09:59:22 +0100

    Use make_unique instead of unique_ptr with new

--- Examples/Algorithms/TrackFinding/src/HoughTransformSeeder.cpp
+++ Examples/Algorithms/TrackFinding/src/HoughTransformSeeder.cpp
@@ -281,9 +281,9 @@ ActsExamples::ProcessCode ActsExamples::HoughTransformSeeder::execute(
     const auto hough_title =
         std::format("event_{:06}_{:02};q/p_{{T}} [1/GeV];#varphi [rad]",
                     ctx.eventNumber, subregion);
-    auto hough_hist = std::unique_ptr<TH2S>(
-        new TH2S(hough_name.c_str(), hough_title.c_str(), m_cfg.houghHistSize_y,
-                 m_bins_y.data(), m_cfg.houghHistSize_x, m_bins_x.data()));
+    auto hough_hist = std::make_unique<TH2S>(
+        hough_name.c_str(), hough_title.c_str(), m_cfg.houghHistSize_y,
+        m_bins_y.data(), m_cfg.houghHistSize_x, m_bins_x.data());
 
     for (unsigned y = 0; y < m_cfg.houghHistSize_y; y++) {
       for (unsigned x = 0; x < m_cfg.houghHistSize_x; x++) {
@@ -393,9 +393,9 @@ ActsExamples::ProcessCode ActsExamples::HoughTransformSeeder::execute(
     const auto peaks_title =
         std::format("peaks_{:06}_{:02};q/p_{{T}} [1/GeV];#varphi [rad]",
                     ctx.eventNumber, subregion);
-    auto peaks_hist = std::unique_ptr<TH2S>(
-        new TH2S(peaks_name.c_str(), peaks_title.c_str(), m_cfg.houghHistSize_y,
-                 m_bins_y.data(), m_cfg.houghHistSize_x, m_bins_x.data()));
+    auto peaks_hist = std::make_unique<TH2S>(
+        peaks_name.c_str(), peaks_title.c_str(), m_cfg.houghHistSize_y,
+        m_bins_y.data(), m_cfg.houghHistSize_x, m_bins_x.data());
 
     const auto all_peaks = slidingWindowPeaks(m_houghHist, m_cfg.slidingWindow);
     ACTS_DEBUG(std::format("Found {} peaks", all_peaks.size()));
