9f1e40697ee5534b575493ca660e08b123462f0a
Krzysztof Cieśla <krzysztof.marcin.ciesla@cern.ch>
2026-03-13 15:12:28 +0100

    Extend q/pT to +-800 MeV

--- Examples/Algorithms/TrackFinding/src/HoughTransformSeeder.cpp
+++ Examples/Algorithms/TrackFinding/src/HoughTransformSeeder.cpp
@@ -194,7 +194,8 @@ ActsExamples::HoughTransformSeeder::HoughTransformSeeder(
                (fineBins + fineFactor * (m_cfg.houghHistSize_y - fineBins));
   }
 
-  for (unsigned i = 0; i <= m_cfg.houghHistSize_y; i++) {
+  m_bins_y.push_back(m_cfg.yMin);
+  for (unsigned i = 1; i <= m_cfg.houghHistSize_y; i++) {
     if (m_cfg.binning == Binning::EqudistantQoverPt) {
       m_bins_y.push_back(
           unquant(m_cfg.yMin, m_cfg.yMax, m_cfg.houghHistSize_y, i));
@@ -556,10 +557,6 @@ static inline double unquant(double min, double max, unsigned nSteps,
 static inline double unquantSteps(double previous, double stepSize,
                                   unsigned nSteps, unsigned from,
                                   unsigned iStep) {
-  if (iStep == 0) {
-    return -1;
-  }
-
   const unsigned half = nSteps / 2;
   const float factor = 2;
   if (iStep <= from / 2 || iStep > nSteps - from / 2) {
@@ -575,10 +572,6 @@ static inline double unquantSteps(double previous, double stepSize,
 static inline double unquantFinerCentral(double previous, double stepSize,
                                          unsigned nSteps, unsigned from,
                                          double factor, unsigned iStep) {
-  if (iStep == 0) {
-    return -1;
-  }
-
   const unsigned half = nSteps / 2;
   if (iStep <= half - from / 2 || iStep > nSteps - half + from / 2) {
     return previous + factor * stepSize;
